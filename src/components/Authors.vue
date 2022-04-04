8646fc0aebdfc3267ad2c96408960b8e3deed9be
Zvinn <77742224+Zvinn@users.noreply.github.com>
2022-04-04 14:14:46 +0200

    Add files via upload

---
+++ src/components/Authors.vue
@@ -0,0 +1,23 @@
+<template>
+ <div id="authors-table">
+    <table>
+        <tbody>
+            <tr v-for="author in authorsSource" :key="author.id">
+                <td>{{ author.name }}</td>
+                <td>{{ author.email }}</td>
+                <td>{{ author.phone }}</td>
+            </tr>
+        </tbody>
+    </table>
+ </div>
+</template>
+
+<script>
+  export default {
+    name: 'author-table',
+    props: {
+      authorsSource: Array,
+    },
+  }
+</script>
+<style scoped></style>
