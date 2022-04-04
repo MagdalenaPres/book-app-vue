f7850fb05b9e0b29b25e11ed86238910d9bfe38d
Zvinn <77742224+Zvinn@users.noreply.github.com>
2022-04-04 14:13:54 +0200

    Add files via upload

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+ <div id="app" class="small-container">
+ <h1>Autorzy</h1>
+  <authors-table />
+ </div>
+ <author-table :authorsSource="authors"/>
 </template>
 
+<script>
+ import Authors from "../components/Authors.vue"
+ export default {
+  name: 'app',
+  components: {
+    Authors,
+  },
+  data() {
+    return {
+      authors: [
+        {
+          id: 1,
+          name: 'Adam Słodowy',
+          email: 'adam.slodowy@zrobtosam.pl',
+          phone: '+48 787 774 664'
+        },
+        {
+          id: 2,
+          name: 'Michał Studencki',
+          email: 'ms@student.pwr.edu.pl',
+          phone: '+48 600 565 454'
+        },
+        {
+          id: 3,
+          name: 'Kamila Napokaz',
+          email: 'kami2003@h2.pl',
+          phone: '+48 609 554 987'
+        },
+      ] 
+    }
+  },
+ }
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+ button {
+ background: #009435;
+ border: 1px solid #009435;
+ }
+ .small-container {
+ max-width: 1200px;
+ }
 </style>
